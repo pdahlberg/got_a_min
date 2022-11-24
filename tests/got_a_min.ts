962891337a78e07636a7008c08bc326975a2940c
Pär Dahlberg <per.dahlberg@gmail.com>
2022-11-24 17:39:10 +0000

    Setting up with Anchor.

---
+++ tests/got_a_min.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { GotAMin } from "../target/types/got_a_min";
+
+describe("got_a_min", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.GotAMin as Program<GotAMin>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
