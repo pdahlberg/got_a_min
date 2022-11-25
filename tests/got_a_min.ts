cc05836f8acd8847f1d9a56f03a74ffd2179c40e
Pär Dahlberg <per.dahlberg@gmail.com>
2022-11-25 16:58:36 +0000

    wip

--- tests/got_a_min.ts
+++ tests/got_a_min.ts
@@ -11,10 +11,9 @@ describe("got_a_min", () => {
   const programProvider = program.provider as anchor.AnchorProvider;
 
   it("Produce one", async () => {
-    const player = anchor.web3.Keypair.generate();
     const resource = anchor.web3.Keypair.generate();
 
-    const tx = await program.methods
+    await program.methods
       .produce()
       .accounts({
         resource: resource.publicKey,
@@ -23,39 +22,10 @@ describe("got_a_min", () => {
       })
       .signers([resource])
       .rpc();
-    console.log("Your transaction signature", tx);
 
     let result = await program.account.resource.fetch(resource.publicKey);
+    
     expect(result.amount.toNumber()).to.equal(1);
   });
-
-  /*it("Produce several", async () => {
-    // Add your test here.
-    const tx1 = await program.methods.produce().rpc();
-    console.log("Your transaction signature", tx1);
-
-    const tx2 = await program.methods.produce().rpc();
-    console.log("Your transaction signature", tx2);
-  });*/
-
-  /*it("Produce several", async () => {
-    await provider.connection.confirmTransaction(
-        await provider.connection.requestAirdrop(donator.publicKey, 10000000000),
-        "confirmed"
-      );
-
-      const tx = await program.rpc.sendDonation(new anchor.BN(100), {
-        accounts: {
-            baseAccount: baseAccount.publicKey,
-            user: donator.publicKey,
-        },
-        signers: [donator],
-      });
-
-      const balance = await program.account.baseAccount.getAccountInfo(donator.publicKey);
-    console.log(balance);
-      expect(balance.lamports.toString()).equal("100");
-    });
-  });*/
 
 });
