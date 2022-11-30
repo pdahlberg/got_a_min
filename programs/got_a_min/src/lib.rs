78361cb85c0febb13073c9cd53a69b4c0dfd7d61
Pär Dahlberg <per.dahlberg@gmail.com>
2022-11-30 15:17:13 +0000

    First steps (#1)
    What: Produce two different resources.

--- programs/got_a_min/src/lib.rs
+++ programs/got_a_min/src/lib.rs
@@ -2,14 +2,66 @@ use anchor_lang::prelude::*;
 
 declare_id!("5kdCwKP8D1ciS9xyc3zRp1PaUcyD2yiBFkgBr8u3jn3K");
 
+#[error_code]
+pub enum ErrorCode2 {
+    #[msg("This error is just for fun.")]
+    ErrorForFun,
+}
+
 #[program]
 pub mod got_a_min {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn init(ctx: Context<InitResource>, name: String) -> Result<()> {
+        let resource: &mut Account<Resource> = &mut ctx.accounts.resource;
+        let owner: &Signer = &ctx.accounts.owner;
+
+        resource.owner = *owner.key;
+        resource.amount = 0;
+        resource.name = name;
+
+        Ok(())
+    }
+
+    pub fn produce(ctx: Context<ProduceResource>) -> Result<()> {
+        let resource: &mut Account<Resource> = &mut ctx.accounts.resource;
+
+        resource.amount += 1;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitResource<'info> {
+    #[account(init, payer = owner, space = Resource::LEN)]
+    pub resource: Account<'info, Resource>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ProduceResource<'info> {
+    #[account(mut)]
+    pub resource: Account<'info, Resource>,
+}
+
+#[account]
+pub struct Resource {
+    pub owner: Pubkey,
+    pub amount: i64,
+    pub name: String,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const OWNER_LENGTH: usize = 32;
+const AMOUNT_LENGTH: usize = 8;
+const NAME_LENGTH: usize = 16 * 4;
+
+impl Resource {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + OWNER_LENGTH
+        + AMOUNT_LENGTH 
+        + NAME_LENGTH;
+}
--- tests/got_a_min.ts
+++ tests/got_a_min.ts
@@ -1,16 +1,71 @@
 import * as anchor from "@project-serum/anchor";
+import chai from 'chai';
+import { expect } from 'chai';
 import { Program } from "@project-serum/anchor";
 import { GotAMin } from "../target/types/got_a_min";
 
 describe("got_a_min", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.GotAMin as Program<GotAMin>;
+  const programProvider = program.provider as anchor.AnchorProvider;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Init resource", async () => {
+    const resource = anchor.web3.Keypair.generate();
+
+    let result = await init(program, resource, "A");
+    
+    expect(result.amount.toNumber()).to.equal(0);
+    expect(result.owner.toBase58).to.equal(programProvider.wallet.publicKey.toBase58);
   });
+
+  it("Produce 1 of resource A", async () => {
+    const resource = anchor.web3.Keypair.generate();
+    await init(program, resource, 'A');
+
+    let result = await produce(program, resource);
+
+    expect(result.amount.toNumber()).to.equal(1);
+    expect(result.name).to.equal('A');
+  });
+
+  it("Produce 1 of resource B", async () => {
+    const resource = anchor.web3.Keypair.generate();
+    await init(program, resource, 'B');
+
+    let result = await produce(program, resource);
+
+    expect(result.amount.toNumber()).to.equal(1);
+    expect(result.name).to.equal('B');
+  });
+
 });
+
+async function init(program: Program<GotAMin>, resource, name: string) {
+  const programProvider = program.provider as anchor.AnchorProvider;
+
+  await program.methods
+    .init(name)
+    .accounts({
+      resource: resource.publicKey,
+      owner: programProvider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers(resource)
+    .rpc();
+    
+    return await program.account.resource.fetch(resource.publicKey);
+}
+
+async function produce(program: Program<GotAMin>, resource) {
+  const programProvider = program.provider as anchor.AnchorProvider;
+
+  await program.methods
+    .produce()
+    .accounts({
+      resource: resource.publicKey,
+    })
+    .rpc();
+
+  return await program.account.resource.fetch(resource.publicKey);
+}
