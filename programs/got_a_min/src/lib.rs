40a93c9f123e73862a2232f49f2a8bd70a37e20d
Pär Dahlberg <per.dahlberg@gmail.com>
2022-12-06 18:18:28 +0000

    Resource with required input resources can be produced.

--- programs/got_a_min/src/lib.rs
+++ programs/got_a_min/src/lib.rs
@@ -3,30 +3,99 @@ use anchor_lang::prelude::*;
 declare_id!("5kdCwKP8D1ciS9xyc3zRp1PaUcyD2yiBFkgBr8u3jn3K");
 
 #[error_code]
-pub enum ErrorCode2 {
-    #[msg("This error is just for fun.")]
-    ErrorForFun,
+pub enum ValidationError {
+    #[msg("Resource has too many inputs defined.")]                             ResourceInputMax,
+    #[msg("Missing resource input amount.")]                                    MissingResourceInputAmount,
+    #[msg("Missing resource account.")]                                         MissingResource,
+    #[msg("Input resource not supplied to production.")]                        InputResourceNotSupplied,
+    #[msg("Input resource 1 not supplied to production.")]                      InputResource1NotSupplied,
+    #[msg("Input resource 2 not supplied to production.")]                      InputResource2NotSupplied,
+    #[msg("Input resource amount is too low.")]                                 InputResourceAmountTooLow,
+    #[msg("Trying stuff out and failing quite deliberately.")]                  ExperimentalError,
 }
 
 #[program]
 pub mod got_a_min {
     use super::*;
 
-    pub fn init(ctx: Context<InitResource>, name: String) -> Result<()> {
+    pub fn init_resource(ctx: Context<InitResource>, name: String, inputs: Vec<Pubkey>, input_amounts: Vec<i64>) -> Result<()> {
         let resource: &mut Account<Resource> = &mut ctx.accounts.resource;
         let owner: &Signer = &ctx.accounts.owner;
 
         resource.owner = *owner.key;
         resource.amount = 0;
         resource.name = name;
+        resource.input = inputs;
+        resource.input_amount = input_amounts;
+
+        require!(resource.input.len() <= INPUT_MAX_SIZE, ValidationError::ResourceInputMax);
+        require!(resource.input.len() == resource.input_amount.len(), ValidationError::MissingResourceInputAmount);
+
+        Ok(())
+    }
+
+    pub fn init_producer(ctx: Context<InitProducer>, resource_id: Pubkey, production_rate: i64) -> Result<()> {
+        let producer: &mut Account<Producer> = &mut ctx.accounts.producer;
+        let owner: &Signer = &ctx.accounts.owner;
+
+        producer.owner = *owner.key;
+        producer.resource_id = resource_id;
+        producer.production_rate = production_rate;
 
         Ok(())
     }
 
     pub fn produce(ctx: Context<ProduceResource>) -> Result<()> {
+        let producer = &ctx.accounts.producer;
         let resource: &mut Account<Resource> = &mut ctx.accounts.resource;
 
-        resource.amount += 1;
+        resource.amount += producer.production_rate;
+
+        require!(resource.input.is_empty(), ValidationError::ResourceInputMax);
+
+        Ok(())
+    }
+
+    pub fn produce_with_one_input(ctx: Context<ProduceResourceWith1Input>) -> Result<()> {
+        let producer = &ctx.accounts.producer;
+        let resource_to_produce: &mut Account<Resource> = &mut ctx.accounts.resource_to_produce;
+        let resource_input: &mut Account<Resource> = &mut ctx.accounts.resource_input;
+
+        let input_exists = resource_to_produce.input.iter().position(|input| input.key().eq(&resource_input.key()));
+        require!(input_exists.is_some(), ValidationError::InputResourceNotSupplied);
+
+        let index = input_exists.unwrap();
+        let input_amount = resource_to_produce.input_amount[index];
+        require!(resource_input.amount >= input_amount, ValidationError::InputResourceAmountTooLow);
+
+        resource_to_produce.amount += producer.production_rate;
+        resource_input.amount -= input_amount;
+
+        Ok(())
+    }
+
+    pub fn produce_with_two_inputs(ctx: Context<ProduceResourceWith2Inputs>) -> Result<()> {
+        let producer = &ctx.accounts.producer;
+        let resource_to_produce: &mut Account<Resource> = &mut ctx.accounts.resource_to_produce;
+        let resource_input_1: &mut Account<Resource> = &mut ctx.accounts.resource_input_1;
+        let resource_input_2: &mut Account<Resource> = &mut ctx.accounts.resource_input_2;
+
+        let input_pos_1 = resource_to_produce.input.iter().position(|input| input.key().eq(&resource_input_1.key()));
+        require!(input_pos_1.is_some(), ValidationError::InputResource1NotSupplied);
+        let input_pos_2 = resource_to_produce.input.iter().position(|input| input.key().eq(&resource_input_2.key()));
+        require!(input_pos_2.is_some(), ValidationError::InputResource2NotSupplied);
+
+        let index_1 = input_pos_1.unwrap();
+        let input_1_amount = resource_to_produce.input_amount[index_1];
+        require!(resource_input_1.amount >= input_1_amount, ValidationError::InputResourceAmountTooLow);
+
+        let index_2 = input_pos_2.unwrap();
+        let input_2_amount = resource_to_produce.input_amount[index_2];
+        require!(resource_input_2.amount >= input_2_amount, ValidationError::InputResourceAmountTooLow);
+
+        resource_to_produce.amount += producer.production_rate;
+        resource_input_1.amount -= input_1_amount;
+        resource_input_2.amount -= input_2_amount;
 
         Ok(())
     }
@@ -42,9 +111,57 @@ pub struct InitResource<'info> {
 }
 
 #[derive(Accounts)]
+pub struct InitProducer<'info> {
+    #[account(init, payer = owner, space = Producer::LEN)]
+    pub producer: Account<'info, Producer>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
 pub struct ProduceResource<'info> {
     #[account(mut)]
+    pub producer: Account<'info, Producer>,
+    #[account(mut)]
     pub resource: Account<'info, Resource>,
+}
+
+#[derive(Accounts)]
+pub struct ProduceResourceWith1Input<'info> {
+    #[account(mut)]
+    pub producer: Account<'info, Producer>,
+    #[account(mut)]
+    pub resource_to_produce: Account<'info, Resource>,
+    #[account(mut)]
+    pub resource_input: Account<'info, Resource>,
+}
+
+#[derive(Accounts)]
+pub struct ProduceResourceWith2Inputs<'info> {
+    #[account(mut)]
+    pub producer: Account<'info, Producer>,
+    #[account(mut)]
+    pub resource_to_produce: Account<'info, Resource>,
+    #[account(mut)]
+    pub resource_input_1: Account<'info, Resource>,
+    #[account(mut)]
+    pub resource_input_2: Account<'info, Resource>,
+}
+
+#[account]
+pub struct Producer {
+    pub owner: Pubkey,
+    pub resource_id: Pubkey,
+    pub production_rate: i64,
+}
+
+impl Producer {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH  // owner
+        + PUBLIC_KEY_LENGTH  // resource_id
+        + PRODUCTION_RATE_LENGTH
+        + INPUT_LENGTH;
 }
 
 #[account]
@@ -52,16 +169,24 @@ pub struct Resource {
     pub owner: Pubkey,
     pub amount: i64,
     pub name: String,
+    pub input: Vec<Pubkey>,
+    pub input_amount: Vec<i64>,
 }
-
-const DISCRIMINATOR_LENGTH: usize = 8;
-const OWNER_LENGTH: usize = 32;
-const AMOUNT_LENGTH: usize = 8;
-const NAME_LENGTH: usize = 16 * 4;
 
 impl Resource {
     const LEN: usize = DISCRIMINATOR_LENGTH
-        + OWNER_LENGTH
-        + AMOUNT_LENGTH 
-        + NAME_LENGTH;
+        + PUBLIC_KEY_LENGTH
+        + AMOUNT_LENGTH
+        + NAME_LENGTH 
+        + INPUT_LENGTH
+        + INPUT_AMOUNT_LENGTH;          
 }
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const PRODUCTION_RATE_LENGTH: usize = 8;
+const AMOUNT_LENGTH: usize = 8;
+const NAME_LENGTH: usize = 16 * 4;
+const INPUT_MAX_SIZE: usize = 2;
+const INPUT_LENGTH: usize = PUBLIC_KEY_LENGTH * INPUT_MAX_SIZE;
+const INPUT_AMOUNT_LENGTH: usize = 8 * INPUT_MAX_SIZE;
--- tests/got_a_min.ts
+++ tests/got_a_min.ts
@@ -1,8 +1,12 @@
 import * as anchor from "@project-serum/anchor";
-import chai from 'chai';
-import { expect } from 'chai';
-import { Program } from "@project-serum/anchor";
+//import { PublicKey, Keypair } from "@solana/web3.js";
+import { assert, expect } from 'chai';
+import { assertion, promise } from 'chai-as-promised';
+import { AnchorError, Program } from "@project-serum/anchor";
 import { GotAMin } from "../target/types/got_a_min";
+import { publicKey } from "@project-serum/anchor/dist/cjs/utils";
+
+type KP = anchor.web3.Keypair;
 
 describe("got_a_min", () => {
   // Configure the client to use the local cluster.
@@ -13,39 +17,130 @@ describe("got_a_min", () => {
   it("Init resource", async () => {
     const resource = anchor.web3.Keypair.generate();
 
-    let result = await init(program, resource, "A");
+    let result = await initResource(program, resource, "A", []);
     
+    expect(result.owner.toBase58()).to.equal(programProvider.wallet.publicKey.toBase58());
     expect(result.amount.toNumber()).to.equal(0);
-    expect(result.owner.toBase58).to.equal(programProvider.wallet.publicKey.toBase58);
+  });
+
+  it("Init resource with input", async () => {
+    const resource = anchor.web3.Keypair.generate();
+    let [resourceA, _] = await createResource(program, "A", []);
+
+    let result = await initResource(program, resource, "B", [[resourceA as KP, 1]]);
+    
+    expect(result.owner.toBase58()).to.equal(programProvider.wallet.publicKey.toBase58());
+    expect(result.amount.toNumber()).to.equal(0);
+  });
+
+  it("Init producer", async () => {
+    const producer = anchor.web3.Keypair.generate();
+    const resource = anchor.web3.Keypair.generate();
+    await initResource(program, resource, "A", []);
+
+    let result = await initProducer(program, producer, resource, 1);
+    
+    expect(result.owner.toBase58()).to.equal(programProvider.wallet.publicKey.toBase58());
+    expect(result.productionRate.toNumber()).to.equal(1);
+    expect(result.resourceId.toBase58()).to.equal(resource.publicKey.toBase58());
   });
 
   it("Produce 1 of resource A", async () => {
-    const resource = anchor.web3.Keypair.generate();
-    await init(program, resource, 'A');
+    let [resource, _] = await createResource(program, 'A', []);
+    let [producer, __] = await createProducer(program, resource, 1);
 
-    let result = await produce(program, resource);
+    let result = await produce_without_input(program, producer, resource);
 
-    expect(result.amount.toNumber()).to.equal(1);
     expect(result.name).to.equal('A');
+    expect(result.amount.toNumber()).to.equal(1);
   });
 
-  it("Produce 1 of resource B", async () => {
-    const resource = anchor.web3.Keypair.generate();
-    await init(program, resource, 'B');
+  it("Produce 2 of resource B", async () => {
+    let [resource, _] = await createResource(program, 'B', []);
+    let [producer, __] = await createProducer(program, resource, 2);
 
-    let result = await produce(program, resource);
+    let result = await produce_without_input(program, producer, resource);
 
-    expect(result.amount.toNumber()).to.equal(1);
     expect(result.name).to.equal('B');
+    expect(result.amount.toNumber()).to.equal(2);
+  });
+
+  it("Produce resource B with input A fails when A is empty", async () => {
+    let [resourceA, _1] = await createResource(program, 'A', []);
+    let [resourceB, _3] = await createResource(program, 'B', [[resourceA, 1]]);
+    let [producerB, _4] = await createProducer(program, resourceB, 2);
+
+    // await expect(stuff(program, producerB, resourceB, resourceA)).should.be.rejectedWith("I AM THE EXPECTED ERROR");
+    try {
+      await produce_with_1_input(program, producerB, resourceB, resourceA);
+      
+      assert(false, "Expected to fail");
+    } catch(e) {
+      assertAnchorError(e, "InputResourceAmountTooLow");
+    }
+
+  });
+
+  it("Produce 1 resource B from 2 A", async () => {
+    let [resourceA, _1] = await createResource(program, 'A', []) as [KP, any];
+    let [producerA, _2] = await createProducer(program, resourceA, 2);
+    let [resourceB, _3] = await createResource(program, 'B', [[resourceA, 2]]);
+    let [producerB, _4] = await createProducer(program, resourceB, 1);
+    await produce_without_input(program, producerA, resourceA);
+
+    let result = await produce_with_1_input(program, producerB, resourceB, resourceA);
+    let inputResult = await program.account.resource.fetch(resourceA.publicKey);
+
+    expect(result.name).to.equal('B');
+    expect(result.amount.toNumber()).to.equal(1);
+    expect(inputResult.amount.toNumber()).to.equal(0);    
+  });
+
+  it("Produce 1 resource C from 1 A + 1 B", async () => {
+    let [resourceA, _1] = await createResource(program, 'A', []) as [KP, any];
+    let [producerA, _2] = await createProducer(program, resourceA, 1);
+    let [resourceB, _3] = await createResource(program, 'B', []) as [KP, any];
+    let [producerB, _4] = await createProducer(program, resourceB, 1);
+    let [resourceC, _5] = await createResource(program, 'C', [[resourceA, 1], [resourceB, 1]]);
+    let [producerC, _6] = await createProducer(program, resourceC, 1);
+    await produce_without_input(program, producerA, resourceA);
+    await produce_without_input(program, producerB, resourceB);
+
+    let result = await produce_with_2_inputs(program, producerC, resourceC, resourceA, resourceB);
+    let inputAResult = await program.account.resource.fetch(resourceA.publicKey);
+    let inputBResult = await program.account.resource.fetch(resourceB.publicKey);
+
+    expect(result.name).to.equal('C');
+    expect(result.amount.toNumber(), "Resource C should be produced").to.equal(1);
+    expect(inputAResult.amount.toNumber(), "Input A should be consumed").to.equal(0);    
+    expect(inputBResult.amount.toNumber(), "Input B should be consumed").to.equal(0);
   });
 
 });
 
-async function init(program: Program<GotAMin>, resource, name: string) {
+function assertAnchorError(error: any, errorName: String) {
+  expect(error).to.be.instanceOf(AnchorError);
+  let anchorError: AnchorError = error;
+  expect(anchorError.error.errorCode.code).to.equal(errorName);
+}
+
+async function createResource(program: Program<GotAMin>, name: string, inputs) {
+  const resource: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+  return [resource, await initResource(program, resource, name, inputs)];
+}
+
+async function initResource(program: Program<GotAMin>, resource, name: string, inputs: [anchor.web3.Keypair, Number][]) {
   const programProvider = program.provider as anchor.AnchorProvider;
 
+  let publicKeyInputs = [];
+  let amountInputs = [];
+  inputs.forEach(tuple => {
+    publicKeyInputs.push(tuple[0].publicKey);
+    amountInputs.push(new anchor.BN(tuple[1].toFixed()));
+  });
+
   await program.methods
-    .init(name)
+    .initResource(name, publicKeyInputs, amountInputs)
     .accounts({
       resource: resource.publicKey,
       owner: programProvider.wallet.publicKey,
@@ -54,18 +149,71 @@ async function init(program: Program<GotAMin>, resource, name: string) {
     .signers(resource)
     .rpc();
     
-    return await program.account.resource.fetch(resource.publicKey);
+  return await program.account.resource.fetch(resource.publicKey);
 }
 
-async function produce(program: Program<GotAMin>, resource) {
+async function createProducer(program: Program<GotAMin>, resource, productionRate) {
+  const producer = anchor.web3.Keypair.generate();
+  return [producer, await initProducer(program, producer, resource, productionRate)];
+}
+
+async function initProducer(program: Program<GotAMin>, producer, resource, productionRate) {
+  const programProvider = program.provider as anchor.AnchorProvider;
+
+  await program.methods
+    .initProducer(resource.publicKey, new anchor.BN(productionRate))
+    .accounts({
+      producer: producer.publicKey,
+      owner: programProvider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers(producer)
+    .rpc();
+    
+    return await program.account.producer.fetch(producer.publicKey);
+}
+
+async function produce_without_input(program: Program<GotAMin>, producer, resource, inputResources = []) {
   const programProvider = program.provider as anchor.AnchorProvider;
 
   await program.methods
     .produce()
     .accounts({
+      producer: producer.publicKey,
       resource: resource.publicKey,
     })
     .rpc();
 
   return await program.account.resource.fetch(resource.publicKey);
+}
+
+async function produce_with_1_input(program: Program<GotAMin>, producer, resourceToProduce, resourceInput) {
+  const programProvider = program.provider as anchor.AnchorProvider;
+
+  await program.methods
+    .produceWithOneInput()
+    .accounts({
+      producer: producer.publicKey,
+      resourceToProduce: resourceToProduce.publicKey,
+      resourceInput: resourceInput.publicKey,      
+    })
+    .rpc();
+
+  return await program.account.resource.fetch(resourceToProduce.publicKey);
+}
+
+async function produce_with_2_inputs(program: Program<GotAMin>, producer, resourceToProduce, resourceInput1, resourceInput2) {
+  const programProvider = program.provider as anchor.AnchorProvider;
+
+  await program.methods
+    .produceWithTwoInputs()
+    .accounts({
+      producer: producer.publicKey,
+      resourceToProduce: resourceToProduce.publicKey,
+      resourceInput1: resourceInput1.publicKey,
+      resourceInput2: resourceInput2.publicKey,
+    })
+    .rpc();
+
+  return await program.account.resource.fetch(resourceToProduce.publicKey);
 }
