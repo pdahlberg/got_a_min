36df531e020b349eff48deb6b70a3e13c0460fd0
Pär Dahlberg <per.dahlberg@gmail.com>
2023-01-17 21:35:50 +0000

    wip

--- programs/got_a_min/src/state/producer.rs
+++ programs/got_a_min/src/state/producer.rs
@@ -5,10 +5,12 @@ pub struct Producer {
     pub owner: Pubkey,
     pub resource_id: Pubkey,
     pub location_id: Pubkey,
+    pub target_storage_id: Pubkey,
     pub output_rate: i64,   // Produce this many units per [processing_duration]. 
     pub processing_duration: i64,   // Solana time unit (usually 400-415ms)
     pub awaiting_units: i64,    // This amount can be claimed after waiting [processing_duration] * [awaiting_units] seconds.
     pub claimed_at: i64,
+    pub producer_type: ProducerType,
 }
 
 impl Producer {
@@ -16,6 +18,7 @@ impl Producer {
         + PUBLIC_KEY_LENGTH  // owner
         + PUBLIC_KEY_LENGTH  // resource_id
         + PUBLIC_KEY_LENGTH  // location_id
+        + PUBLIC_KEY_LENGTH  // storage_id
         + OUTPUT_RATE_LENGTH
         + PROCESSING_DURATION_LENGTH
         + AWAITING_UNITS_LENGTH
@@ -24,6 +27,12 @@ impl Producer {
     pub fn size(&self) -> i64 {
         1
     }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum ProducerType {
+    Producer,
+    Sender,
 }
 
 const AWAITING_UNITS_LENGTH: usize = 8;
