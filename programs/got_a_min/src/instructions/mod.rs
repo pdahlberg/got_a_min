31e340003cc7ffe215addabf8327125a3445c57d
Pär Dahlberg <per.dahlberg@gmail.com>
2023-01-18 20:38:11 +0000

    wip

--- programs/got_a_min/src/instructions/mod.rs
+++ programs/got_a_min/src/instructions/mod.rs
@@ -1,9 +1,11 @@
+pub use game::*;
 pub use location::*;
 pub use resource::*;
 pub use processor::*;
 pub use storage::*;
 pub use stuff::*;
 
+pub mod game;
 pub mod location;
 pub mod resource;
 pub mod processor;
